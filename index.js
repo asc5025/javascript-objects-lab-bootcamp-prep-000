ba15e7077eecca048eea85f52200f9c1ee32fcfc
Albert Chun <albert.s.chun@gmail.com>
2018-08-02 03:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
 }
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
