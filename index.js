7dc3bcf053226e95299ba6a0121f8d324660894d
Albert Chun <albert.s.chun@gmail.com>
2018-08-02 04:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  key = Object.assign({}, object);
-  delete object[key];
-  return object;
+  return Object.assign({}, object);
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
